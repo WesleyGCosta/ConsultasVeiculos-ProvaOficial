ec9090790baa7a35eec2b07b29d98cd0c4195941
Wesley Gomes <wesleygomescosta825@gmail.com>
2021-06-24 19:54:03 -0400

    Primeiro commit

---
+++ src/Web/WebApplication/Models/VeiculoViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class VeiculoViewModel
+    {
+        [HiddenInput]
+        public int VeiculoId { get; set; }
+
+        [Required(ErrorMessage = "*Campo Obrigatório!")]
+        public string Marca { get; set; }
+
+        [Required(ErrorMessage = "*Campo Obrigatório!")]
+        public string Modelo { get; set; }
+
+        [Required(ErrorMessage = "*Campo Obrigatório!")]
+        public int Ano { get; set; }
+
+        [Required(ErrorMessage = "*Campo Obrigatório!")]
+        public double Quilometragem { get; set; }
+    }
+}
